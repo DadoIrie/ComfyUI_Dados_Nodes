5d53be1a81ee6479c8988ec0df6d5e13fb7b0aad
dadoirie <damir.krupic@gmail.com>
2025-09-09 04:33:40 +0200

    Remove unused Wildcard Selector modal initialization code

--- web/common/js/DN_WildcardSelectorComposerV2/modal_init.js
+++
@@ -1,6 +0,0 @@
-import { WildcardsModal } from './WildcardsModal.js';
-
-export async function showWildcardSelectorModal(node, constants) {
-    const modal = new WildcardsModal(node, constants);
-    await modal.show();
-}
